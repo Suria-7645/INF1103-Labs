14050e4e5964b42db52d1d8bf19aa20595674bde
Suria <suriapraksh7@gmail.com>
2026-09-04 14:38:24 +0800

    added dynamic profile

--- lab1/hello.py
+++ lab1/hello.py
@@ -1,21 +1,38 @@
-print("===================")
-print("Welcome Here!")
-print("My first post")
-print("===================")
+#Activity 1
+# print("===================")
+# print("Welcome Here!")
+# print("My first post")
+# print("===================")
 
-username = "sur1a.xo"
-bio = "Gambling Addict"
-followers = 365
+# #Activity 2
+#  username = "sur1a.xo"
+# bio= "Gambling Addict"
+# followers = 365
 
+# #Activity 3
+# print("Username: " + username)
+# print("Bio: " + bio)
+# print("Followers: " , followers)
+
+# followers +=50
+# print("Day 1 :" , followers)
+
+# followers += 20
+# print("Day 2 :" , followers)
+
+# followers -=10
+# print("Day 3 :" , followers)
+
+#Activity 4
+
+username = input("Enter your username: ")
+age = int(input("Enter your age: "))
+category = input("Enter your content category: ")
+
+print("\nInstagram Profile")
+print("===================")
 print("Username: " + username)
-print("Bio: " + bio)
-print("Followers: " , followers)
+print("Age: " ,age)
+print("Category: " + category)
 
-followers +=50
-print("Day 1 :" , followers)
 
-followers += 20
-print("Day 2 :" , followers)
-
-followers -=10
-print("Day 3 :" , followers)
