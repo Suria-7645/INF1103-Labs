7eac149ec2b6613d825145bbfba7618caee47b56
Suria <suriapraksh7@gmail.com>
2026-09-04 13:10:14 +0800

    added Day1,2,3 Trackers

--- lab1/hello.py
+++ lab1/hello.py
@@ -9,4 +9,13 @@ followers = 365
 
 print("Username: " + username)
 print("Bio: " + bio)
-print("Followers: " , followers)   
+print("Followers: " , followers)
+
+followers +=50
+print("Day 1 :" , followers)
+
+followers += 20
+print("Day 2 :" , followers)
+
+followers -=10
+print("Day 3 :" , followers)
