578303a232d192cd4200c08b672d7a1ab8a1afa0
Suria <suriapraksh7@gmail.com>
2026-09-04 13:05:32 +0800

    Added username,bio and followers

--- lab1/hello.py
+++ lab1/hello.py
@@ -2,3 +2,11 @@ print("===================")
 print("Welcome Here!")
 print("My first post")
 print("===================")
+
+username = "sur1a.xo"
+bio = "Gambling Addict"
+followers = 365
+
+print("Username: " + username)
+print("Bio: " + bio)
+print("Followers: " , followers)   
